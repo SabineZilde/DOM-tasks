4af4ed967f87f57e8877d68391689ebffd1d5d6c
SabineZilde <sabine.zilde@protonmail.ch>
2021-04-01 21:06:14 +0300

    Second attempt

--- Task2/js/main.js
+++ Task2/js/main.js
@@ -1,10 +1,12 @@
 // 2. Write a JavaScript function to get the values of First and Last 
 // name of the following form. 
 
-function getFormvalue() {
-    // submitEvent.preventDefault();
-    const form = document.getElementById('form1').children;
-    // const formChildren = form.children;
-    const formValue = form.value;
-    console.log(formValue);    
+function getFormvalue(submitEvent) {
+    submitEvent.preventDefault();
+    const fName = document.getElementById('fName').value;
+    const lName = document.getElementById('lName').value;
+
+    console.log(fName);
+    console.log(lName);
+
 }
