16ead6c164029147d20965fff1e4b8aa3f164385
LasmaMaksimovica <78431887+LasmaMaksimovica@users.noreply.github.com>
2021-04-01 21:04:48 +0300

    initial commit

---
+++ Task3/js/main.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to set the background color of a paragraph.
+
+function setBackgroundColor() {
+    let pElementBackgroundColor = document.getElementById("p");
+    pElementBackgroundColor.style.backgroundColor = "red";
+}
